591667a569ab477bd2e1668b85be3688c7b9d2e1
sethficke <sethficke@yahoo.com>
2017-08-26 16:28:51 -0500

    Refactor data access into dll

---
+++ DataImport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataImport
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
